b5238c52738ed9ab290cc36989ec25633aa45085
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-03-04 10:32:58 +0000

    Add ProductController and DTO

---
+++ src/NetCoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAPI.Dto;
+
+namespace NetCoreAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if (id <=0)
+            {
+                return BadRequest("Invalid id");
+            }
+
+            var product = new ProductResponse
+            {
+                Id = id,
+                Name = "Test",
+                Price = 100,
+                StockQuantity = 100,
+                SequenceNumber = 1,
+            };
+
+            return Ok(product);
+        }
+    }
+}
---
+++ src/NetCoreAPI/Dto/ProductResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace NetCoreAPI.Dto
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int SequenceNumber { get; set; }
+    }
+}
