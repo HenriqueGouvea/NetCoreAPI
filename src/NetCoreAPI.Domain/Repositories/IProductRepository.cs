2159cacd432cfbf1b43d808f05e0cade4d3864b9
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-03-04 19:58:49 +0000

    Add repository layer

---
+++ src/NetCoreAPI.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using NetCoreAPI.Domain.Models;
+
+namespace NetCoreAPI.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddAsync(Product product);
+
+        Task DeleteAsync(int id);
+
+        Task<Product> GetAllAsync(int? pageNumber = null, int? pageSize = null);
+
+        Task<Product> GetByIdAsync(int id);
+
+        Task UpdateAsync(Product product);
+
+        Task CommitAsync();
+    }
+}
---
+++ src/NetCoreAPI.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using NetCoreAPI.Domain.Models;
+using NetCoreAPI.Domain.Repositories;
+
+namespace NetCoreAPI.Repository.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> AddAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetAllAsync(int? pageNumber = null, int? pageSize = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
